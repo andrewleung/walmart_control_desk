06f508d9bbeb0ef952568161792d0f0592aa475f
Andrew Leung <625315+andrewleung@users.noreply.github.com>
2026-07-28 09:18:47 +0800

    Configure default file storage for production seeding

--- config/settings.py
+++ config/settings.py
@@ -63,6 +63,9 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 if not DEBUG:
     STORAGES = {
+        "default": {
+            "BACKEND": "django.core.files.storage.FileSystemStorage",
+        },
         "staticfiles": {
             "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
         },
